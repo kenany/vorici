43451fd2303a7d079d082fae8a66fe20b338c03f
Kenan Yildirim <kenan@kenany.me>
2018-03-09 10:02:29 -0500

    webpack: simplify autoprefixer config

--- webpack.config.js
+++ webpack.config.js
@@ -45,12 +45,8 @@ module.exports = {
                 require('postcss-flexbugs-fixes'),
                 autoprefixer({
                   browsers: [
-                    '>1%',
-                    'last 4 versions',
-                    'Firefox ESR',
-                    'not ie < 9'
-                  ],
-                  flexbox: 'no-2009'
+                    '> 5%'
+                  ]
                 })
               ]
             }
