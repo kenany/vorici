536c26a00d539067e4908dca63a2e592b3bc8cad
Kenan Yildirim <kenan@kenany.me>
2019-06-03 19:01:27 -0400

    deps: autoprefixer@9.6.0

--- webpack.config.js
+++ webpack.config.js
@@ -50,11 +50,7 @@ module.exports = {
               ident: 'postcss',
               plugins: () => [
                 require('postcss-flexbugs-fixes'),
-                autoprefixer({
-                  browsers: [
-                    '> 5%'
-                  ]
-                })
+                autoprefixer()
               ]
             }
           }
