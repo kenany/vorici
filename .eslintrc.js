f0cf4a3f70c334960d51bd29cbf946ee8ec1c02d
Kenan Yildirim <kenan@kenany.me>
2020-10-25 12:30:42 -0400

    chore(deps): use @babel/eslint-parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,15 @@
 module.exports = {
   extends: ['@kenan', 'plugin:react/recommended'],
   plugins: ['react'],
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true
+    }
+  },
+  settings: {
+    'import/parsers': {
+      espree: ['.js']
     }
   }
 };
