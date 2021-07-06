55e938b2ca7b10672e113f0ba4b2468185df2c0c
Kenan Yildirim <kenan@kenany.me>
2021-07-06 10:15:25 -0400

    chore(deps): webpack@5.42.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,73 @@
 'use strict';
 
-const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const LodashWebpackPlugin = require('lodash-webpack-plugin');
 const path = require('path');
-const process = require('process');
 const webpack = require('webpack');
 
-module.exports = {
-  devtool: 'cheap-module-source-map',
-  entry: path.resolve(__dirname, 'index.js'),
-  output: {
-    path: path.resolve(__dirname, 'out'),
-    filename: 'static/js/bundle.js',
-    chunkFilename: 'static/js/[name].chunk.js',
-    publicPath: '/',
-    devtoolModuleFilenameTemplate: (info) =>
-      path.resolve(info.absoluteResourcePath).replace(/\\/g, '/')
-  },
-  optimization: {
-    minimize: process.env.NODE_ENV === 'production'
-  },
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  resolve: {
-    alias: {
-      lodash: 'lodash-es'
+module.exports = (env, argv) => {
+  const mode = argv.mode || 'development';
+  return {
+    devtool: 'cheap-module-source-map',
+    entry: path.resolve(__dirname, 'index.js'),
+    mode,
+    output: {
+      path: path.resolve(__dirname, 'out'),
+      filename: 'static/js/bundle.js',
+      chunkFilename: 'static/js/[name].chunk.js',
+      publicPath: '/',
+      devtoolModuleFilenameTemplate: (info) =>
+        path.resolve(info.absoluteResourcePath).replace(/\\/g, '/')
     },
-    extensions: ['.js', '.json', '.jsx']
-  },
-  module: {
-    strictExportPresence: true,
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: [/node_modules/]
+    optimization: {
+      minimize: mode === 'production'
+    },
+    resolve: {
+      alias: {
+        lodash: 'lodash-es'
       },
-      {
-        test: /\.css$/,
-        use: [
-          require.resolve('style-loader'),
-          {
-            loader: require.resolve('css-loader'),
-            options: { importLoaders: 1 }
-          },
-          {
-            loader: require.resolve('postcss-loader'),
-            options: {
-              ident: 'postcss',
-              plugins: () => [
-                require('postcss-flexbugs-fixes'),
-                autoprefixer()
-              ]
+      extensions: ['.js', '.json', '.jsx']
+    },
+    module: {
+      strictExportPresence: true,
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: [/node_modules/]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            require.resolve('style-loader'),
+            {
+              loader: require.resolve('css-loader'),
+              options: { importLoaders: 1 }
+            },
+            {
+              loader: require.resolve('postcss-loader'),
+              options: {
+                postcssOptions: {
+                  plugins: [
+                    'autoprefixer',
+                    'postcss-flexbugs-fixes'
+                  ]
+                }
+              }
             }
-          }
-        ]
-      }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: true,
+        template: path.resolve(__dirname, 'public', 'index.html')
+      }),
+      new LodashWebpackPlugin(),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(mode)
+      })
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: path.resolve(__dirname, 'public', 'index.html')
-    }),
-    new LodashWebpackPlugin(),
-    new webpack.NamedModulesPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': `'${process.env.NODE_ENV}'`
-    })
-  ]
+  };
 };
